8ab07a333f3464e26752d3b9c3b6e14ea2ed75f3
Mariano Aloi <mariano@aloi.com.br>
2022-10-12 21:03:40 -0300

    initial

---
+++ src/native-test/java/br/com/aloi/AluguelRobotIT.java
@@ -0,0 +1,8 @@
+package br.com.aloi;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class AluguelRobotIT extends AluguelRobotTest {
+    // Execute the same tests but in packaged mode.
+}
